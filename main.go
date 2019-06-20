a58143b544d45df686ede1a049c0024629e3c910
DanLud <dan.ludvigsson@abilia.se>
2019-06-20 14:58:15 +0200

    Added main and starting code

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+// Database
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "gobeeruser"
+	password = "thisisthepassword"
+	dbname   = "beerdb"
+)
+
+// Tasting Struct (Model)
+type Tasting struct {
+	ID    string `json:"id"`
+	Theme string `json:"theme"`
+	Beers []Beer `json:"beers"`
+	Users []User `json:"users"`
+	User  *User  `json:"user"`
+}
+
+// Beer struct
+type Beer struct {
+	ID        int    `json:"id"`
+	Name      string `string:"name"`
+	Tastingid int    `json:"tastingid"`
+}
+
+// Common user
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+// Scorecard for a user
+type ScoreCard struct {
+	ID       string   `json:"id"`
+	Beers    []Beer   `json:"beers"`
+	Comments []string `json:"comments"`
+	Points   []int    `json:"points"`
+}
+
+func main() {
+	// Init mux router
+	router := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	router.HandleFunc("/api/v1/user", addUser).Methods("POST")
+	router.HandleFunc("/api/v1/users", getUsers).Methods("GET")
+	router.HandleFunc("/api/v1/user", getUser).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+// Add new user with username
+func addUser(respWriter http.ResponseWriter, request *http.Request) {
+	respWriter.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	json.NewEncoder(respWriter).Encode(user)
+}
+
+// Return all current users
+func getUsers(respWriter http.ResponseWriter, request *http.Request) {
+}
+
+// Get user with specific id
+func getUser(respWriter http.ResponseWriter, request *http.Request) {
+	respWriter.Header().Set("Content-Type", "application/json")
+	keys, ok := request.URL.Query()["id"]
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'id' is missing")
+		return
+	}
+	id, err := strconv.Atoi(keys[0])
+	if err != nil {
+		panic(err)
+	}
+	user := getUserFromDb(id)
+	json.NewEncoder(respWriter).Encode(user)
+}
+
+func getUserFromDb(id int) User {
+	// Init database
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	var username string
+	getUserStatement := "SELECT * FROM users WHERE id=$1;"
+	row := db.QueryRow(getUserStatement, id)
+	switch err := row.Scan(&id, &username); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+	case nil:
+		fmt.Println(id, username)
+	default:
+		panic(err)
+	}
+	var user User
+	user.ID = id
+	user.Username = username
+	return user
+}
